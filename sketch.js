faaddf9090244aa0f6f920dd6cdca2eda9b794a8
elisamanzoni <37081924+elisamanzoni@users.noreply.github.com>
2019-11-11 23:08:57 +0100

    cover

--- sketch.js
+++ sketch.js
@@ -47,8 +47,8 @@ function draw() {
 
   var character_position = myMap.latLngToPixel(_latitude,_longitude);
   var charmander_position = myMap.latLngToPixel(_latitude*0.99999,_longitude*0.9998);
-  image(character, character_position.x, character_position.y, character.width, character.height);
-  image(charmander, charmander_position.x, charmander_position.y, charmander.width/6, charmander.height/6);
+  image(character, character_position.x, character_position.y, character.width*1.5, character.height*1.5);
+  image(charmander, charmander_position.x, charmander_position.y, charmander.width/4.5, charmander.height/4.5);
   imageMode(CENTER);
   image(tab, windowWidth/2, 5.3*(windowHeight/6), tab.width, tab.height);
 
@@ -56,13 +56,13 @@ function draw() {
 noStroke();
 fill('#989898');
 rectMode(CENTER);
-rect(windowWidth-125,80,190,140);
+rect(windowWidth-200,100,220,160);
 
 stroke('#505957');
 strokeWeight(8);
 fill('black');
 rectMode(CENTER);
-rect(windowWidth-125,80,150,100);
+rect(windowWidth-200,100,180,120);
 
 
 
@@ -75,9 +75,9 @@ rect(windowWidth-125,80,150,100);
   textAlign(CENTER);
   fill('lightGreen');
   textFont('VT323');
-  textSize(30);
-  text('Time',windowWidth-150,70);
-  text(formattedTime,windowWidth-125,100);
+  textSize(40);
+  text('Time',windowWidth-230,90);
+  text(formattedTime,windowWidth-200,130);
 
 
 
